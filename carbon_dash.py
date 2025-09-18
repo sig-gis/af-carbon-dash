70d6c2ada94f79718f8ae5b7520a7d53d3a7b08e
Noah Giebink <ngiebink@sig-gis.com>
2025-09-18 13:00:53 -0500

    clean up

--- carbon_dash.py
+++ carbon_dash.py
@@ -1,16 +1,10 @@
 import streamlit as st
 
 home = st.Page("pages/0_home.py", title="🏠 Home")
-planting = st.Page("pages/1_planting.py", title="🌲 Planting Scenario")
-carbon_est = st.Page("pages/2_carbon_est.py", title="📈 Carbon Units Estimate")
-credits = st.Page("pages/3_credits.py", title="📈 Credits")
-maps = st.Page("pages/1_5_map_planting.py", title="🌲 Project Builder")
+project = st.Page("pages/1_5_map_planting.py", title="🌲 Project Builder")
 pg = st.navigation([
     home,
-    maps,
-    # planting,
-    # carbon_est,
-    credits
+    project,
 ])
 st.set_page_config(page_title="American Forests Dashboard", page_icon="🌲")
 pg.run()
