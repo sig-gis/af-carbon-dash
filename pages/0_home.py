4a1925257aecc7b5acc7c25e245d2f41a36c0b36
Noah Giebink <ngiebink@sig-gis.com>
2025-09-18 22:05:38 -0500

    update intro/instructions

--- pages/0_home.py
+++ pages/0_home.py
@@ -2,11 +2,19 @@ import streamlit as st
 
 st.set_page_config(layout="wide", page_title="Home", page_icon="🌲")
 
-st.title("🌲 Welcome to the Carbon Dashboard")
+st.title("Welcome to the Carbon Dashboard")
 st.markdown("""
-Use the sidebar to begin exploring the app:
+Navigate to 🌲 Project Builder in the sidebar to begin exploring the app. 
 
-- **Planting Scenario**: Design your forest and estimate total carbon
-- **Carbon Units Estimate**: Estimate certified carbon units under different protocols
-- **Credits**: Estimate revenue
+Inside 🌲 Project Builder, advance through tabs to design your project and estimate outcomes.
+- **Site Selection Map**: Select the FVS Variant Location geometry which contains your project location. Your selected FVS Variant Location will auto-populate helpful planting parameter defaults, including species mix, for you. 
+- **Planting Scenario**: Estimate certified carbon units under different protocols.
+    - *Planting Parameters*: design your reforestation plan (survival percentage, site index, and species mix) and estimate \*FVS results in real time. 
+    - *Carbon Estimates*: estimate ERTs (tons CO₂e/acre) under different protocols.
+    - *Credits*: Customize financial factors to estimate net project revenue.
+            
+
+\*Real-time estimates based on regression models trained on FVS runs.
 """)
+
+st.subheader("Warning: This is pre-release software which may contain errors and incomplete or misleading information. It is intended for UX experimentation only.")
