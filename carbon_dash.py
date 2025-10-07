178c460fafb8c20a92231419e5cec4f4bfa8d187
rizayeva <48497511+rizayeva@users.noreply.github.com>
2025-10-07 14:30:38 -0400

    FAQs

--- carbon_dash.py
+++ carbon_dash.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 home = st.Page("pages/0_home.py", title="🏠 Home")
 project = st.Page("pages/1_project_builder.py", title="🌲 Project Builder")
+faq = st.Page("pages/5_faq.py", title="❓ Frequently Asked Questions")
 pg = st.navigation([
     home,
     project,
+    faq,
 ])
 st.set_page_config(page_title="American Forests Dashboard", page_icon="🌲")
 pg.run()
---
+++ pages/5_faq.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(layout="wide", page_title="Home", page_icon="❓")
+
+st.title("❓ Frequently Asked Questions")
+st.markdown("""
+
+**1. How are the Full verification costs calculated?**
+
+The following formula is used to calculate the full verification costs that employs user inputs in the Financial Options drop-down menu.
+
+$$
+\\mathit{Full\ verification\ costs} = 
+\\mathit{Number\ of\ plots} \\times \\mathit{Cost\ per\ CFI\ plot} \\times (1 + \\mathit{Anticipated\ inflation})
+$$
+
+**2. How?**
+
+**3. How?**
+
+**4. How?**
+
+
+""")
+
+st.subheader("Note:")
