0d0bdb473f3e3786666c03ab2ae5b40eef2940d4
rizayeva <48497511+rizayeva@users.noreply.github.com>
2025-10-15 10:21:47 -0400

    Update 5_faq.py

--- pages/5_faq.py
+++ pages/5_faq.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide", page_title="Home", page_icon="🌲")
 
-st.title("❓ Frequently Asked Questions")
+st.title("❓Frequently Asked Questions")
 
 # Define the FAQs as a list of dictionaries
 faqs = [
