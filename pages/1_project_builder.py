f77f7f48d4a15c81bbc10a6477a3a671f4bb4c49
elijah-dalton <edalton@sig-gis.com>
2026-07-06 10:36:26 -0400

    updated planting design button

--- pages/1_project_builder.py
+++ pages/1_project_builder.py
@@ -347,74 +347,122 @@ if st.session_state.active_tab == "Site Selection Map":
 
     # Fill the button placeholder now that click state is up to date.
     # Show navigation buttons below the map.
-    if st.session_state.get("selected_variant"):
-        # st.divider()
+    # if st.session_state.get("selected_variant"):
+    #     # st.divider()
 
-        # col1, col2 = st.columns(2)
+    #     # col1, col2 = st.columns(2)
 
-        # with col1:
-        #     if st.button(
-        #         "➡️ Planting Design",
-        #         use_container_width=True,
-        #         help=H("site.button_forward_to_planting"),
-        #         type="primary",
-        #     ):
-        #         st.session_state.active_tab = "Planting Design"
-        #         st.rerun()
+    #     # with col1:
+    #     #     if st.button(
+    #     #         "➡️ Planting Design",
+    #     #         use_container_width=True,
+    #     #         help=H("site.button_forward_to_planting"),
+    #     #         type="primary",
+    #     #     ):
+    #     #         st.session_state.active_tab = "Planting Design"
+    #     #         st.rerun()
 
-        # with col2:
-        #     if st.button(
-        #         "🧮 Solver",
-        #         use_container_width=True,
-        #         help=H("site.button_forward_to_solver"),
-        #     ):
-        #         st.session_state.active_tab = "Solver"
-        #         st.rerun()
-        # Show navigation buttons below the map.
-        st.divider()
+    #     # with col2:
+    #     #     if st.button(
+    #     #         "🧮 Solver",
+    #     #         use_container_width=True,
+    #     #         help=H("site.button_forward_to_solver"),
+    #     #     ):
+    #     #         st.session_state.active_tab = "Solver"
+    #     #         st.rerun()
+    #     # Show navigation buttons below the map.
+    #     st.divider()
 
-        location_selected = bool(st.session_state.get("selected_variant"))
+    #     location_selected = bool(st.session_state.get("selected_variant"))
 
-        disabled_msg = (
-            "Select a location first using Step 1: click the map, enter coordinates/address, "
-            "or upload a shapefile/GeoJSON."
-        )
+    #     disabled_msg = (
+    #         "Select a location first using Step 1: click the map, enter coordinates/address, "
+    #         "or upload a shapefile/GeoJSON."
+    #     )
 
-        col1, col2 = st.columns(2)
+    #     col1, col2 = st.columns(2)
 
-        with col1:
+    #     with col1:
+    #         if st.button(
+    #             "➡️ Planting Design",
+    #             use_container_width=True,
+    #             help=H("site.button_forward_to_planting") if location_selected else disabled_msg,
+    #             type="primary",
+    #             disabled=not location_selected,
+    #         ):
+    #             st.session_state.active_tab = "Planting Design"
+    #             st.rerun()
+
+    #     with col2:
+    #         if st.button(
+    #             "🧮 Solver",
+    #             use_container_width=True,
+    #             help=H("site.button_forward_to_solver") if location_selected else disabled_msg,
+    #             disabled=not location_selected,
+    #         ):
+    #             st.session_state.active_tab = "Solver"
+    #             st.rerun()
+    # Show navigation buttons below the map.
+    st.divider()
+
+    location_selected = bool(st.session_state.get("selected_variant"))
+
+    disabled_msg = (
+        "Select a location first using Step 1: click the map, enter coordinates/address, "
+        "or upload a shapefile/GeoJSON."
+    )
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if location_selected:
             if st.button(
                 "➡️ Planting Design",
                 use_container_width=True,
-                help=H("site.button_forward_to_planting") if location_selected else disabled_msg,
+                help=H("site.button_forward_to_planting"),
                 type="primary",
-                disabled=not location_selected,
             ):
                 st.session_state.active_tab = "Planting Design"
                 st.rerun()
+        else:
+            st.button(
+                "➡️ Planting Design",
+                use_container_width=True,
+                help=disabled_msg,
+                type="primary",
+                disabled=True,
+            )
 
-        with col2:
+    with col2:
+        if location_selected:
             if st.button(
                 "🧮 Solver",
                 use_container_width=True,
-                help=H("site.button_forward_to_solver") if location_selected else disabled_msg,
-                disabled=not location_selected,
+                help=H("site.button_forward_to_solver"),
             ):
                 st.session_state.active_tab = "Solver"
                 st.rerun()
+        else:
+            st.button(
+                "🧮 Solver",
+                use_container_width=True,
+                help=disabled_msg,
+                disabled=True,
+            )
 
-    if reset_button:
-        for key in [
-            "upload_file",
-            "uploaded_geojson_str",
-            "uploaded_tooltip_fields",
-            "last_upload",
-            "upload_auto_selected"
-        ]:
-            if key in st.session_state:
-                del st.session_state[key]
+        if reset_button:
+            for key in [
+                "upload_file",
+                "uploaded_geojson_str",
+                "uploaded_tooltip_fields",
+                "last_upload",
+                "upload_auto_selected"
+            ]:
+                if key in st.session_state:
+                    del st.session_state[key]
 
-        st.rerun()
+            st.rerun()
+            
 elif st.session_state.active_tab == "Planting Design":
     col1, col2 = st.columns([8, 3])
 
